866d3c971ce9016a31bd6bc0b59d41e24bb0da7f
Simon Mott <simon@simonmott.co.uk>
2019-05-19 23:55:19 +0100

    config loading and db connection

--- app/Main.php
+++ app/Main.php
@@ -2,23 +2,60 @@
 
 class Main {
 
-	private $theme = "default";
+	private $config = null;
+	private $db = null;
+
+	private function defaults() {
+		$config['THEME'] = 'default';
+		return $config;
+	}
 
 	public function __construct() {
 		spl_autoload_register(array($this,'classLoader'));
+
+		$this->loadConfig();
+		$this->initDB();
 	}
 
 	private function classLoader($class) {
-		require_once(__DIR__ . '/'.$class.'.php');
+		require_once(BASE_DIR. '/app/'.$class.'.php');
 	}
 	
 	private function loadConfig() {
-		// Some kind of config check
+		if (file_exists(BASE_DIR . '/config.php')) {
+			require_once(BASE_DIR. '/config.php');
+			$this->config = array_merge($this->defaults(), $config);
+		} else {
+			//TODO handle this nicer
+			$this->fatalErr('no config');
+		}
+	}
+
+	private function initDB() {
+		$this->db = new MySQL($this->config['SQL_HOSTNAME'],
+							$this->config['SQL_PORT'],
+							$this->config['SQL_USERNAME'],
+							$this->config['SQL_PASSWORD'],
+							$this->config['SQL_DATABASE']
+						);
+		$db = $this->db;
+		if (!is_object($db->getHandle())) {
+			$this->fatalErr('invalid db creds');
+		}
+
+	}
+
+	private function fatalErr($string) {
+		//TODO make this theme stuff
+		//TODO some kind of error codes so theme can do wording
+		echo $string;
+		die();
 	}
 	
+	public function getDB() {
+		return $this->db;
+	}
 	
 }
+
 ?>
-
-
-
--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php
 
+define("BASE_DIR", __DIR__);
 require_once(__DIR__ . '/app/Main.php');
 
 $main = new Main();
