a38b9c9f7f558bd7d7d183b90a2cb282e970e8f5
Simon Mott <simon@simonmott.co.uk>
2019-05-20 02:27:34 +0100

    reorder config defaults
    For the theme specifically anyway, if we can't load a config file
    then this will never actually get set for the class variable
    and thus wont be available when I get around to rendering errors
    nicely.

--- app/Main.php
+++ app/Main.php
@@ -5,11 +5,6 @@ class Main {
 	private $config = null;
 	private $db = null;
 
-	private function defaults() {
-		$config['THEME'] = 'default';
-		return $config;
-	}
-
 	public function __construct() {
 		spl_autoload_register(array($this,'classLoader'));
 
@@ -22,9 +17,14 @@ class Main {
 	}
 	
 	private function loadConfig() {
+		// Load defaults here.
+		// If for any reason we cant load our config file, we'll need to know these to display an error.
+		$config['THEME'] = 'default';
+
+		$this->config = $config;
 		if (file_exists(__DIR__ . '/../config.php')) {
 			require_once(__DIR__. '/../config.php');
-			$this->config = array_merge($this->defaults(), $config);
+			$this->config = array_merge($this->config, $config);
 			//TODO check for required config entries
 		} else {
 			//TODO handle this nicer
@@ -49,6 +49,7 @@ class Main {
 	private function fatalErr($string) {
 		//TODO make this theme stuff
 		//TODO some kind of error codes so theme can do wording
+		var_dump($this->config);
 		echo $string;
 		die();
 	}
