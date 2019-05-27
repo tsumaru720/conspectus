e22e3fb09813f8ca1753ff00fa039d8d31f25af7
Simon Mott <simon@simonmott.co.uk>
2019-05-27 23:18:19 +0100

    move logic into twig template

--- themes/default/app/loading_error.php
+++ themes/default/app/loading_error.php
@@ -7,15 +7,6 @@ class Document extends Theme {
 	public function __construct(&$main, &$twig, $vars) {
 
 		$vars['page_title'] = $this->pageTitle;
-		
-		switch ($vars['error_code']) {
-			case 'NO_CFG':
-				$vars['string'] = 'Unable to load config file';
-				break;
-			case 'DB_403':
-				$vars['string'] = 'Unable to connect to database server; Please check the credentials provided';
-				break;
-		}
 
 		$this->vars = $vars;
 		$this->document = $twig->load('loading_error.html');
