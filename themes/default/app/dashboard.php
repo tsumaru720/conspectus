b5f1f3048235cc1319d66cef775407ae5786a18d
Simon Mott <simon@simonmott.co.uk>
2019-06-04 01:42:55 +0100

    improve readability

--- themes/default/app/dashboard.php
+++ themes/default/app/dashboard.php
@@ -11,18 +11,37 @@ class Document extends Theme {
 
 		$this->db = $main->getDB();
 
-		$q = $this->db->query("SELECT SUM(deposit_value) AS deposit_total, SUM(asset_value) AS asset_total, SUM(asset_value - deposit_value) AS gain, DATE_FORMAT(epoch, '%b %Y') AS period, EXTRACT(YEAR_MONTH FROM epoch) AS yearMonth FROM asset_log GROUP BY period, yearMonth ORDER BY yearMonth ASC");
+		$q = $this->db->query("SELECT SUM(deposit_value) AS deposit_total,
+								SUM(asset_value) AS asset_total,
+								SUM(asset_value - deposit_value) AS gain,
+								DATE_FORMAT(epoch, '%b %Y') AS period,
+								EXTRACT(YEAR_MONTH FROM epoch) AS yearMonth
+								FROM asset_log
+								GROUP BY period, yearMonth
+								ORDER BY yearMonth ASC");
+
 		while ($period = $this->db->fetch($q)) {
-			$period['deposit_str'] = '£'.number_format($period['deposit_total'], 2);
-			$period['asset_str'] = ($period['asset_total'] < 0 ? '-£'.number_format($period['asset_total'] * -1,2) : '£'.number_format($period['asset_total'],2));
-			$period['gain_str'] = ($period['gain'] < 0 ? '-£'.number_format($period['gain'] * -1,2) : '£'.number_format($period['gain'],2));
+			// Calculate Gain Difference
+			$period['delta'] = 0;
+			if (isset($last)) {
+				$period['delta'] = ($period['gain'] - $last['gain']);
+			}
+
 			$vars['periodData'][] = $period;
 			$last = $period;
 		}
+
+		$last['deposit_str'] = $this->prettify($last['deposit_total']);
+		$last['asset_str'] = $this->prettify($last['asset_total']);
+		$last['gain_str'] = $this->prettify($last['gain']);
 		$vars['mostRecent'] = $last;
-		$this->debug($last);
+
 		$this->vars = $vars;
 		$this->document = $twig->load('dashboard.html');
+	}
+
+	private function prettify($value) {
+		return ($value < 0 ? '-£'.number_format($value * -1, 2) : '£'.number_format($value, 2));
 	}
 
 }
