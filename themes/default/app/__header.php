d3622e7c94b23c4d1e247c28c73bcd9a8342749f
Simon Mott <simon@simonmott.co.uk>
2019-06-08 01:19:17 +0100

    Add asset list to menu + minor tweaks

--- themes/default/app/__header.php
+++ themes/default/app/__header.php
@@ -2,7 +2,15 @@
 
 class Header extends Theme {
 
+	private $db = null;
+
 	public function __construct(&$main, &$twig, $vars) {
+
+		$this->db = $main->getDB();
+		$q = $this->db->query("SELECT * from asset_list ORDER BY description ASC");
+		while ($asset = $this->db->fetch($q)) {
+			$vars['assets'][] = $asset;
+		}
 
 		$this->vars = $vars;
 		$this->document = $twig->load('__header.html');
