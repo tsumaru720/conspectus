583f7e653f7a0ca3f1d641689359e2eef4d3a485
Simon Mott <simon@simonmott.co.uk>
2019-06-22 00:53:17 +0100

    sort by value for easier comparison in polarArea. Also remove comments

--- themes/default/app/breakdown.php
+++ themes/default/app/breakdown.php
@@ -26,8 +26,7 @@ class Document extends Theme {
 
 		while ($log = $this->db->fetch($logQuery)) {
 			$vars['labels'][$log['yearMonth']] = $log['period'];
-			
-			//$vars['log'][$log['yearMonth']][] = array('name' => $log['description'], 'value' => $log['asset_value']);
+
 			$vars['log'][$log['asset_id']]['name'] = $log['description'];
 			$vars['log'][$log['asset_id']]['data'][$log['yearMonth']] = $log['asset_value'];
 			
@@ -35,6 +34,9 @@ class Document extends Theme {
 			$last['log'][$log['asset_id']]['value'] = $log['asset_value'];
 		}
 
+		usort($last['log'], function($a, $b) {
+			return $b['value'] - $a['value'];
+		});
 		$vars['mostRecent'] = $last['log'];
 
 		foreach ($vars['log'] as $key => $entry) {
@@ -45,71 +47,9 @@ class Document extends Theme {
 				}
 			}
 		}
-		
-//$this->dump($vars);
 
 		$this->vars = $vars;
 		$this->document = $twig->load('breakdown.html');
 	}
 
 }
-/*
-  ["labels"]=>
-  array(6) {
-    [201901]=>
-    string(8) "Jan 2019"
-    [201902]=>
-    string(8) "Feb 2019"
-    [201903]=>
-    string(8) "Mar 2019"
-    [201904]=>
-    string(8) "Apr 2019"
-    [201905]=>
-    string(8) "May 2019"
-    [201906]=>
-    string(8) "Jun 2019"
-  }
-
-
-  array(14) {
-    [39]=>
-    array(2) {
-      ["name"]=>
-      string(14) "AEGON (iomart)"
-      ["data"]=>
-      array(6) {
-        [201901]=>
-        string(7) "2918.67"
-        [201902]=>
-        string(7) "3159.21"
-        [201903]=>
-        string(7) "3324.69"
-        [201904]=>
-        string(7) "3515.32"
-        [201905]=>
-        string(7) "3742.87"
-        [201906]=>
-        string(7) "3851.30"
-      }
-    }
-    [3]=>
-    array(2) {
-      ["name"]=>
-      string(18) "HSBC Regular Saver"
-      ["data"]=>
-      array(6) {
-        [201901]=>
-        string(6) "750.00"
-        [201902]=>
-        string(7) "1000.00"
-        [201903]=>
-        string(7) "1250.00"
-        [201904]=>
-        string(7) "1500.00"
-        [201905]=>
-        string(7) "1750.00"
-        [201906]=>
-        string(7) "2000.00"
-      }
-    }
-*/
