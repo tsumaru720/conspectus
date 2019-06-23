389365f07347d435ca3dfc9f7c32d6e579ac7ea8
Simon Mott <simon@simonmott.co.uk>
2019-06-24 00:30:28 +0100

    basic update script

---
+++ bin/update.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+HAS_COMPOSER=$(which composer)
+COMPOSER=""
+
+if [ "$HAS_COMPOSER" != "" ]; then
+	COMPOSER="composer"
+else
+	if [ -f "composer.phar" ]; then
+		COMPOSER="./composer.phar"
+	fi
+
+	if [ -f "../composer.phar" ]; then
+		COMPOSER="../../composer.phar"
+	fi
+
+	if [ -f "../../composer.phar" ]; then
+		COMPOSER="../../composer.phar"
+	fi
+
+	if [ "$COMPOSER" == "" ]; then
+		echo "Unable to find composer"
+		exit;
+	fi
+fi
+
+git pull
+$COMPOSER install -d "../"
