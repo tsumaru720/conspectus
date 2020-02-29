9ec1d5cc62829511619a1d0d9a6fc5dc936a6534
Simon Mott <simon@simonmott.co.uk>
2020-02-29 20:34:31 +0000

    search box to filter list (via @shanemcc)

--- themes/default/resources/js/conspectus.js
+++ themes/default/resources/js/conspectus.js
@@ -12,23 +12,56 @@ $('[data-save-state]').each(function() {
 });
 
 $(function() {
-    $('[data-save-state]').each(function() {
-      var element = $(this);
-      var descr = element.attr('id');
-      var state = window.localStorage.getItem(descr + "_collapsed") || "false";
+  $('[data-save-state]').each(function() {
+    var element = $(this);
+    var descr = element.attr('id');
+    var state = window.localStorage.getItem(descr + "_collapsed") || "false";
 
-      if (state == "true") {
-        //Hidden
-        element.collapse('hide');
+    if (state == "true") {
+      //Hidden
+      element.collapse('hide');
+    } else {
+      //Visible
+      element.collapse('show');
+    }
+
+    // increase transition duration for a nicer animation after initial page load.
+    setTimeout(function() {
+      element.css('transition-duration','0.35s')
+    },10);
+
+  });
+
+  $("input[data-search-top]").on('input', function() {
+      var value = $(this).val();
+      var searchTop = $(this).data('search-top');
+
+      if (value == "") {
+        $(searchTop).find("[data-searchable-value]").show();
       } else {
-        //Visible
-        element.collapse('show');
+        var match = new RegExp('^.*' + escapeRegExp(value) + '.*$', 'i');
+
+        $(searchTop).find("[data-searchable-value]").each(function() {
+          var show = false;
+
+          for (val in $(this).data('searchable-value').split(" ")) {
+            if ($(this).data('searchable-value').match(match)) {
+              show = true;
+              break;
+            }
+          }
+
+          if (show) {
+            $(this).show();
+          } else {
+            $(this).hide();
+          }
+        });
       }
+  });
 
-      // increase transition duration for a nicer animation after initial page load.
-      setTimeout(function() {
-        element.css('transition-duration','0.35s')
-      },10);
-
-    })
 });
+
+function escapeRegExp(str) {
+  return str.replace(/[\-\[\]\/\{\}\(\)\*\+\?\.\\\^\$\|]/g, "\\$&");
+}
