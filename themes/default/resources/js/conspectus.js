bead21fa9dfa5fcff35e4746458f5d37c13d804a
Chris Smith <chris@chameth.com>
2020-02-29 21:34:51 +0000

    Modernise the javascript a little

--- themes/default/resources/js/conspectus.js
+++ themes/default/resources/js/conspectus.js
@@ -1,67 +1,42 @@
 feather.replace()
 
-$('[data-save-state]').each(function() {
-  var element = $(this);
+function setUpMenuItems () {
+  const localStorage = window.localStorage
 
-  element.on('shown.bs.collapse', function () {
-    window.localStorage.setItem(element.attr('id') + "_collapsed", "false");
-  })
-  element.on('hidden.bs.collapse', function () {
-    window.localStorage.setItem(element.attr('id') + "_collapsed", "true");
-  })
-});
+  document.querySelectorAll('[data-save-state]').forEach((el) => {
+    const key = `${el.id}_collapsed`
+    $(el).on('hidden.bs.collapse', () => localStorage.setItem(key, 'true'))
+    $(el).on('shown.bs.collapse', () => localStorage.removeItem(key))
 
-$(function() {
-  $('[data-save-state]').each(function() {
-    var element = $(this);
-    var descr = element.attr('id');
-    var state = window.localStorage.getItem(descr + "_collapsed") || "false";
-
-    if (state == "true") {
-      //Hidden
-      element.collapse('hide');
-    } else {
-      //Visible
-      element.collapse('show');
+    if (!localStorage.getItem(key)) {
+      $(el).collapse('show')
     }
 
-    // increase transition duration for a nicer animation after initial page load.
-    setTimeout(function() {
-      element.css('transition-duration','0.35s')
-    },10);
-
-  });
-
-  $("input[data-search-top]").on('input', function() {
-      var value = $(this).val();
-      var searchTop = $(this).data('search-top');
-
-      if (value == "") {
-        $(searchTop).find("[data-searchable-value]").show();
-      } else {
-        var match = new RegExp('^.*' + escapeRegExp(value) + '.*$', 'i');
-
-        $(searchTop).find("[data-searchable-value]").each(function() {
-          var show = false;
-
-          for (val in $(this).data('searchable-value').split(" ")) {
-            if ($(this).data('searchable-value').match(match)) {
-              show = true;
-              break;
-            }
-          }
-
-          if (show) {
-            $(this).show();
-          } else {
-            $(this).hide();
-          }
-        });
-      }
-  });
-
-});
-
-function escapeRegExp(str) {
-  return str.replace(/[\-\[\]\/\{\}\(\)\*\+\?\.\\\^\$\|]/g, "\\$&");
+    // Increase transition duration for a nicer animation after initial page load.
+    setTimeout(() => {
+      el.style.transitionDuration = '0.35s'
+    }, 10)
+  })
 }
+
+function setUpSearch () {
+  document.querySelectorAll('input[data-search-top]').forEach((searchBar) => {
+    searchBar.addEventListener('input', () => {
+      const query = searchBar.value.toLowerCase()
+      const target = document.querySelector(searchBar.getAttribute('data-search-top'))
+      target.querySelectorAll('[data-searchable-value]').forEach((el) => {
+        const value = el.getAttribute('data-searchable-value').toLowerCase()
+        if (value.includes(query)) {
+          $(el).show()
+        } else {
+          $(el).hide()
+        }
+      })
+    })
+  })
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  setUpMenuItems()
+  setUpSearch()
+})
