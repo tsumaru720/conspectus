b5ab62535d65b39f6488dda5d01a9f7a95645ebd
Greg Holmes <git@greg.holmes.name>
2021-07-04 16:28:23 +0100

    Set PDO to use exceptions, fixes old PHP versions

--- app/MySQL.php
+++ app/MySQL.php
@@ -8,6 +8,7 @@ class MySQL {
     public function __construct($host, $port, $username, $password, $database) {
         try {
             $this->handle = new PDO("mysql:host=$host;port=$port;dbname=$database;charset=utf8mb4", $username, $password);
+            $this->handle->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         }
         catch(PDOException $e) {
             // Handle this ourselves
